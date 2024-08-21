c43d2897afe1b7b9fcfc01a7402ce76dd308bd0b
waleed <waleedashir6@gmail.com>
2024-08-21 15:07:07 +0100

    python ver.

--- MentalHealth/settings.py
+++ MentalHealth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i7t+#$djvveuphvl&6qr===c+_#slgtg3hbpd39i+gfbkpq@(t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
